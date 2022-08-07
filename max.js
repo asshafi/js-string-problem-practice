6cce874e7573f4f285cd90559f042a442c656a07
S M SHAFI <asshafi15@gmail.com>
2022-08-08 01:22:36 +0600

    tallest check

---
+++ max.js
@@ -0,0 +1,36 @@
+const jim = 84;
+const dela = 75;
+const chinku = 87;
+// if (jim > dela) {
+//     console.log('jim will get the cake');
+// }
+// else {
+//     console.log('Dela you will get the cake');
+// }
+
+// if (jim > dela && jim > chinku) {
+//     console.log('Jim will get the cake');
+// }
+// else if (dela > jim && dela > chinku) {
+//     console.log('Dela will get the cake');
+// }
+// else {
+//     console.log('chinku will get the cake');
+// }
+
+Math.max(12, 45,);
+
+function getMaxNumber() {
+    if (jim > dela && jim > chinku) {
+        console.log('Jim will get the cake');
+    }
+    else if (dela > jim && dela > chinku) {
+        console.log('Dela will get the cake');
+    }
+    else {
+        console.log('chinku will get the cake');
+    }
+
+}
+getMaxNumber();
+console.log(getMaxNumber);
---
+++ tallest.js
@@ -0,0 +1,18 @@
+function maxInAarray(numbers) {
+    let largest = numbers[0];
+    for (let i = 0; i < numbers.length; i++){
+       
+        const index = i;
+        const element = numbers[index];
+        console.log(element);
+        if (element > largest) {
+            largest = element;
+        }
+       
+    }
+    return largest;
+}
+
+const heights = [167, 190, 120, 165, 137];
+const tallest = maxInAarray(heights);
+console.log('Tallest person is:',tallest);
