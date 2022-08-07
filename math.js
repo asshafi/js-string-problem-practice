5d1037e973c0e480136ab8bddb5c5438af044702
S M SHAFI <asshafi15@gmail.com>
2022-08-07 13:49:50 +0600

    math practice

---
+++ math.js
@@ -0,0 +1,31 @@
+// const result = Math.pow(3, 8);
+// console.log(result);
+
+// const num1 = 25;
+// const num2 = 45;
+// const gap = Math.abs(num1 - num2);
+// if (gap < 5) {
+//     console.log('you guys can be friend');
+// }
+// else {
+//     console.log('you guys start apart');
+// }
+
+
+// // -----------------------
+// //
+// // -----------------------
+
+// const number = 2.4598;
+// const fullNumber = Math.round(number);
+// // console.log(fullNumber);
+
+// const result2 = Math.ceil(2.11442);
+// const result3 = Math.floor(2.5563);
+// console.log(result2,result3);
+
+// console.log(Math.random());
+for (let i = 0; i < 20; i++){
+    const random = Math.round(Math.random() * 20);
+    console.log(random);
+}
---
+++ split.js
@@ -0,0 +1,11 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki, bosonto kale tomai bolte parini,sada sada kala';
+// const parts = lyrics.split(' ');
+// const sentences = lyrics.split(',');
+// console.log(sentences);
+
+// const chars = lyrics.split('');
+// console.log(chars);
+
+const partial1 = lyrics.slice(5, 8);
+// console.log(partial);
+
